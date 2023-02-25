e052be1a5426be90307e580d34e04d2eadad389f
JesseLiii <jesseli751@gmail.com>
2023-02-25 17:01:25 -0500

    grid colour variatons

--- src/pages/sections/FeatureGrid.tsx
+++ src/pages/sections/FeatureGrid.tsx
@@ -27,14 +27,13 @@ const FeatureGrid: React.FC = () => {
 			<div id='cards'>
 				<div className='card'>
 					<div className='card-content'>
-						<div className='card-image'>
-							<i className='fa-duotone fa-apartment'></i>
-						</div>
 						<div className='card-info-wrapper'>
 							<div className='card-info'>
-								<i className='fa-duotone fa-apartment'></i>
+								{/* <i className='fa-duotone fa-apartment'></i> */}
 								<div className='card-info-title'>
-									<h3>Apartments</h3>
+									<h3 className='text-xl font-bold'>
+										Apartments
+									</h3>
 									<h4>Places to be apart. Wait, what?</h4>
 								</div>
 							</div>
@@ -43,14 +42,13 @@ const FeatureGrid: React.FC = () => {
 				</div>
 				<div className='card'>
 					<div className='card-content'>
-						<div className='card-image'>
-							<i className='fa-duotone fa-unicorn'></i>
-						</div>
 						<div className='card-info-wrapper'>
 							<div className='card-info'>
-								<i className='fa-duotone fa-unicorn'></i>
+								{/* <i className='fa-duotone fa-unicorn'></i> */}
 								<div className='card-info-title'>
-									<h3>Unicorns</h3>
+									<h3 className='text-xl font-bold'>
+										Unicorns
+									</h3>
 									<h4>A single corn. Er, I mean horn.</h4>
 								</div>
 							</div>
@@ -59,14 +57,13 @@ const FeatureGrid: React.FC = () => {
 				</div>
 				<div className='card'>
 					<div className='card-content'>
-						<div className='card-image'>
-							<i className='fa-duotone fa-blender-phone'></i>
-						</div>
 						<div className='card-info-wrapper'>
 							<div className='card-info'>
-								<i className='fa-duotone fa-blender-phone'></i>
+								{/* <i className='fa-duotone fa-blender-phone'></i> */}
 								<div className='card-info-title'>
-									<h3>Blender Phones</h3>
+									<h3 className='text-xl font-bold'>
+										Blender Phones
+									</h3>
 									<h4>These absolutely deserve to exist.</h4>
 								</div>
 							</div>
@@ -75,14 +72,11 @@ const FeatureGrid: React.FC = () => {
 				</div>
 				<div className='card'>
 					<div className='card-content'>
-						<div className='card-image'>
-							<i className='fa-duotone fa-person-to-portal'></i>
-						</div>
 						<div className='card-info-wrapper'>
 							<div className='card-info'>
-								<i className='fa-duotone fa-person-to-portal'></i>
+								{/* <i className='fa-duotone fa-person-to-portal'></i> */}
 								<div className='card-info-title'>
-									<h3>Adios</h3>
+									<h3 className='text-xl font-bold'>Adios</h3>
 									<h4>See you...</h4>
 								</div>
 							</div>
@@ -91,14 +85,13 @@ const FeatureGrid: React.FC = () => {
 				</div>
 				<div className='card'>
 					<div className='card-content'>
-						<div className='card-image'>
-							<i className='fa-duotone fa-person-from-portal'></i>
-						</div>
 						<div className='card-info-wrapper'>
 							<div className='card-info'>
-								<i className='fa-duotone fa-person-from-portal'></i>
+								{/* <i className='fa-duotone fa-person-from-portal'></i> */}
 								<div className='card-info-title'>
-									<h3>I mean hello</h3>
+									<h3 className='text-xl font-bold'>
+										I mean hello
+									</h3>
 									<h4>...over here.</h4>
 								</div>
 							</div>
@@ -107,14 +100,13 @@ const FeatureGrid: React.FC = () => {
 				</div>
 				<div className='card'>
 					<div className='card-content'>
-						<div className='card-image'>
-							<i className='fa-duotone fa-otter'></i>
-						</div>
 						<div className='card-info-wrapper'>
 							<div className='card-info'>
-								<i className='fa-duotone fa-otter'></i>
+								{/* <i className='fa-duotone fa-otter'></i> */}
 								<div className='card-info-title'>
-									<h3>Otters</h3>
+									<h3 className='text-xl font-bold'>
+										Otters
+									</h3>
 									<h4>Look at me, imma cute lil fella.</h4>
 								</div>
 							</div>
