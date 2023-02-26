9bf031b6db3aef7617f42273315bdc3815fcd2ce
JesseLiii <jesseli751@gmail.com>
2023-02-25 21:10:32 -0500

    preconfiged header

--- src/pages/sections/HeaderSection.tsx
+++ src/pages/sections/HeaderSection.tsx
@@ -1,5 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import TitleRotation from '../../components/TitleRotation';
+import Particles from 'react-particles';
+import type { Container, Engine } from 'tsparticles-engine';
+import { loadFull } from 'tsparticles';
+import { particleSettings } from '../../components/Particles.js/particleSettings';
 
 // TypeScript users only add this code
 
@@ -19,10 +23,112 @@ const HeaderSection: React.FC = () => {
 		};
 	}, []);
 
+	const particlesInit = useCallback(async (engine: Engine) => {
+		console.log(engine);
+
+		// you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(engine);
+	}, []);
+
+	const particlesLoaded = useCallback(
+		async (container: Container | undefined) => {
+			await console.log(container);
+		},
+		[]
+	);
+
 	return (
 		<div className='h-screen w-screen w-min-screen flex items-center justify-items-center'>
 			<div id='blob'></div>
 			<div id='blur'></div>
+			<div className='z-10 absolute h-screen w-screen'>
+				<Particles
+					id='tsparticles'
+					init={particlesInit}
+					loaded={particlesLoaded}
+					options={{
+						pauseOnBlur: true,
+						pauseOnOutsideViewport: true,
+						// background: {
+						// 	size: '100vh 100vw',
+						// 	repeat: 'no-repeat',
+						// 	position: 'relative',
+						// 	opacity: 0,
+						// },
+						fullScreen: false,
+						fpsLimit: 120,
+						interactivity: {
+							events: {
+								onClick: {
+									enable: true,
+									mode: 'push',
+								},
+								onHover: {
+									enable: true,
+									mode: 'grab',
+								},
+								resize: true,
+							},
+							modes: {
+								push: {
+									quantity: 4,
+								},
+								repulse: {
+									distance: 200,
+									duration: 0.4,
+								},
+								grab: {
+									distance: 200,
+								},
+							},
+						},
+						particles: {
+							color: {
+								value: '#ffffff',
+							},
+							links: {
+								color: '#ffffff',
+								distance: 150,
+								enable: true,
+								opacity: 0.5,
+								width: 1,
+							},
+							collisions: {
+								enable: true,
+							},
+							move: {
+								direction: 'none',
+								enable: true,
+								outModes: {
+									default: 'bounce',
+								},
+								random: false,
+								speed: 3,
+								straight: false,
+							},
+							number: {
+								density: {
+									enable: true,
+									area: 800,
+								},
+								value: 80,
+							},
+							opacity: {
+								value: 0.5,
+							},
+							shape: {
+								type: 'circle',
+							},
+							size: {
+								value: { min: 1, max: 5 },
+							},
+						},
+						detectRetina: true,
+					}}
+				/>
+			</div>
 
 			<div className='flex-col flex items-center justify-items-center w-full z-10'>
 				<div className='h1 flex-1 align-middle items-center w-fit text-5xl font-bold'>
@@ -30,10 +136,13 @@ const HeaderSection: React.FC = () => {
 						<span className='self-center'>Your</span>
 						<TitleRotation /> doesn’t
 					</div>
-					<div className=' align-middle mb-10 justify-between flex flex-row'>
+					{/* <div className=' align-middle mb-10 justify-between flex flex-row'>
 						<div>fit</div> <div>in</div> <div>files</div>{' '}
 						<div>&</div>
 						<div>folders</div>
+					</div> */}
+					<div className='self-center align-middle mb-10 justify-center flex flex-row'>
+						fit in files & folders
 					</div>
 				</div>
 				<div className='flex-1 w-1/2 mb-10'>
