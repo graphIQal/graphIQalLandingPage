01452a7e00c9b0febadd860bffbcaecbd2303e0f
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2023-04-05 19:02:48 -0400

    colors

--- src/assets/Colours.js
+++ src/assets/Colours.js
@@ -1,9 +1,20 @@
+// export const colours = [
+//   '#4362B1',
+//   '#FE9B42',
+//   '#CAEFAC',
+//   '#F2584B',
+//   '#5FBDE5',
+//   '#F9B3D5',
+//   '#FBC813',
+// ];
+
 export const colours = [
-  '#4362B1',
-  '#FE9B42',
-  '#CAEFAC',
-  '#F2584B',
-  '#5FBDE5',
-  '#F9B3D5',
-  '#FBC813',
+  '#E0C1FF',
+
+  '#FFF492',
+  '#FF92D3',
+  '#FF9292',
+  '#92FFAA',
+  '#92D1FF',
+  '#FFBE92',
 ];
