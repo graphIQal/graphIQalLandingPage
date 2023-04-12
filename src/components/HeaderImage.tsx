e92605f8fd90457772a19345fdf1b92df1830e07
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2023-04-12 08:48:04 -0400

    fixing modal, blog, deploying header

--- src/components/HeaderImage.tsx
+++ src/components/HeaderImage.tsx
@@ -4,37 +4,33 @@ import Header1 from '../assets/images/figmas/Header1.png';
 import Header2 from '../assets/images/figmas/Header2.png';
 
 export const HeaderImage = () => {
-	const left = document.getElementById('left-side');
+  const left = document.getElementById('left-side');
 
-	const handleMove = (e: any) => {
-		console.log(
-			`${Math.max(
-				((e.clientX / window.innerWidth - 1 / 6) * 3) / 2,
-				0
-			)} %`
-		);
+  const handleMove = (e: any) => {
+    console.log(
+      `${Math.max(((e.clientX / window.innerWidth - 1 / 6) * 3) / 2, 0)} %`
+    );
 
-		if (left)
-			left.style.width = `${
-				Math.max(((e.clientX / window.innerWidth - 1 / 6) * 3) / 2, 0) *
-				100
-			}%`;
-	};
+    if (left)
+      left.style.width = `${
+        Math.max(((e.clientX / window.innerWidth - 1 / 6) * 3) / 2, 0) * 100
+      }%`;
+  };
 
-	document.onmousemove = (e) => handleMove(e);
+  document.onmousemove = (e) => handleMove(e);
 
-	document.ontouchmove = (e) => handleMove(e.touches[0]);
+  document.ontouchmove = (e) => handleMove(e.touches[0]);
 
-	return (
-		<div className='w-full mb-3 relative'>
-			<div className='lg:w-2/3 w-[90%] drop-shadow-xl rounded-md border mx-auto relative overflow-hidden '>
-				<img className={''} src={Header1} />
-				<img
-					id='left-side'
-					src={Header2}
-					className='absolute top-0 left-0 lg:h-[60vh] h-full overflow-hidden object-cover object-left'
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className='w-full mb-3 relative'>
+      <div className='lg:w-2/3 w-[90%] drop-shadow-xl rounded-md border mx-auto relative overflow-hidden '>
+        <img className={''} src={Header1} />
+        <img
+          id='left-side'
+          src={Header2}
+          className='absolute top-0 left-0 lg:h-[60vh] h-full overflow-hidden object-cover object-left'
+        />
+      </div>
+    </div>
+  );
 };
